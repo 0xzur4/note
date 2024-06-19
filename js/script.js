61695d3c9b4952036d53be2339b9b470853133a0
Zura Podi <kazuma.madao@gmail.com>
2024-06-19 04:46:25 +0000

    update

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
+// hmm
+'use strict'
+
+
 document.addEventListener('DOMContentLoaded', function() {
     const btnNew = document.querySelector('.btn-new');
 
     btnNew.addEventListener('click', function() {
         const newDiv = document.createElement('div');
-        newDiv.className = 'ui-widget-content';
-        const newP = document.createElement('p');
-        newP.textContent = 'Drag me around';
-        const newTextArea = document.createElement('textarea')
-        newTextArea.style.width = "130px"
-        newTextArea.style.height = "120px"
+        newDiv.style.borderRadius = "12px"
+        newDiv.className = 'ui-widget-content'
+        newDiv.contentEditable = "true"
+        const newP = document.createElement('p')
+        newP.textContent = 'Drag me around'
 
         newDiv.appendChild(newP);
-        newDiv.appendChild(newTextArea);
 
 
         document.querySelector('.note').appendChild(newDiv);
 
-        $(newDiv).draggable();
+        $(newDiv).draggable().resizable();
     });
 });
